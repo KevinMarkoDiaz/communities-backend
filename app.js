6d1d21ef6b035d67c72f647bb662d505311f2c0b
kev dia <kevindi0695@gmail.com>
2025-05-06 19:51:40 -0500

    🚀 Añadidas rutas y controladores para eventos con soporte multicomunidad y multinegocio

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/user.routes.js";
 import communityRoutes from "./routes/community.routes.js";
 import businessRoutes from "./routes/business.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
+import eventRoutes from "./routes/event.routes.js";
 
 import cookieParser from 'cookie-parser';
 
@@ -19,5 +20,6 @@ app.use("/api", userRoutes);
 app.use("/api", communityRoutes);
 app.use("/api", businessRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", eventRoutes);
 
 export default app;
